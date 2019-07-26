02dd0d73c8394486eb16ba38812ce9d7c3eabfe4
haris wedira <hariswediraa@gmail.com>
2019-07-26 16:31:52 +0700

    Editing DOM
    Adding folder images

--- script/app.js
+++ script/app.js
@@ -1,10 +1,14 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const detail = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUi = (data) => {
-    const cityDet = data.cityDet;
-    const weather = data.weather;
+    const {
+        cityDet,
+        weather
+    } = data;
 
     detail.innerHTML = `
     <h5 class="my-3">${cityDet.EnglishName}</h5>
@@ -13,6 +17,18 @@ const updateUi = (data) => {
         <span>${weather.Temperature.Metric.Value}</span>
         <span>&deg;C</span>
     </div>`;
+
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = null;
+    if (weather.IsDayTime) {
+        timeSrc = 'img/day.svg';
+    } else {
+        timeSrc = 'img/night.svg';
+    }
+
+    time.setAttribute('src', timeSrc);
 
     card.classList.remove('d-none');
 };
