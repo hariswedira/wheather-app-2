99279ca19e144bf63ab20e54dc9fdd18e4e61475
haris wedira <hariswediraa@gmail.com>
2019-07-26 13:35:59 +0700

    Getting API

---
+++ script/app.js
@@ -0,0 +1,21 @@
+const cityForm = document.querySelector('form');
+
+const updateCity = async (city) => {
+    const cityDet = await getCity(city);
+    const weather = await getWeather(cityDet.Key);
+
+    return {
+        cityDet,
+        weather
+    };
+};
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    updateCity(city)
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+});
--- script/forecast.js
+++ script/forecast.js
@@ -1,1 +1,36 @@
 const key = 'E26dwgLvGt0nmZnpoBAUAYOd6qpEahAA';
+const card = document.querySelector('.card');
+const detail = document.querySelector('.details')
+
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data;
+
+};
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+// getCity('jakarta').then(data => console.log(data)).catch(err => console.log(err));
+// getWeather('208971').then(data => console.log(data)).catch(err => console.log(err));
+
+// getCity('jakarta')
+//     .then(data => {
+//         return getWeather(data.Key);
+//     }).then(data => console.log(data))
+//     .catch(err => console.log(err));
